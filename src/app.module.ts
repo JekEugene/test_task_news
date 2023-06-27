d66428534681aeea22c0d54971f614cd6dea0d18
Yauheni Naskou <yauheni.naskou@innowise-group.com>
2023-06-27 20:47:58 +0300

    added: typeorm and database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './shared/typeorm/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/shared/typeorm/typeorm.config.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
+  public createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: this.configService.get<string>('DATABASE_HOST'),
+      port: Number(this.configService.get<string>('DATABASE_PORT')),
+      database: this.configService.get<string>('DATABASE_NAME'),
+      username: this.configService.get<string>('DATABASE_USER'),
+      password: this.configService.get<string>('DATABASE_PASSWORD'),
+      entities: ['dist/**/*.entity.{ts,js}'],
+      migrations: ['dist/src/shared/typeorm/migration/*.{ts,js}'],
+      migrationsTableName: 'typeorm_migrations',
+      //logger: 'file',
+      logging: false,
+      autoLoadEntities: true,
+      synchronize: false, // never use TRUE in production!
+    };
+  }
+}
