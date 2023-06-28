42003bae52d9d41db8c8e1efc0c9fdb10179a98a
Yauheni Naskou <yauheni.naskou@innowise-group.com>
2023-06-28 12:15:42 +0300

    added: swagger doc to auth

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -1,4 +1,14 @@
 import { Body, Controller, Delete, Post } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiConflictResponse,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { DefaultErrorResponse } from '../../shared/dto/default-error-response.dto';
+import { DefaultResponse } from '../../shared/dto/default-response.dto';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/sign-in.dto';
 import { SignUpDto } from './dto/sign-up.dto';
@@ -7,16 +17,49 @@ import { SignUpDto } from './dto/sign-up.dto';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({
+    summary: 'signUp',
+  })
+  @ApiCreatedResponse({
+    description: 'sign up completed',
+    type: DefaultResponse,
+  })
+  @ApiBadRequestResponse({
+    description: 'Could not create a user',
+    type: DefaultErrorResponse,
+  })
+  @ApiConflictResponse({
+    description: 'User with provided email already exist',
+    type: DefaultErrorResponse,
+  })
   @Post('sign-up')
   signUp(@Body() signUpDto: SignUpDto) {
     return this.authService.signUp(signUpDto);
   }
 
+  @ApiOperation({
+    summary: 'signIn',
+  })
+  @ApiCreatedResponse({
+    description: 'sign in completed',
+    type: DefaultResponse,
+  })
+  @ApiUnauthorizedResponse({
+    description: 'wrong credentials',
+    type: DefaultErrorResponse,
+  })
   @Post('sign-in')
   signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto);
   }
 
+  @ApiOperation({
+    summary: 'logout',
+  })
+  @ApiOkResponse({
+    description: 'logout completed',
+    type: DefaultResponse,
+  })
   @Delete('logout')
   logout() {
     // return this.authService.logout();
--- src/api/auth/dto/sign-up.dto.ts
+++ src/api/auth/dto/sign-up.dto.ts
@@ -1,3 +1,24 @@
-import { SignInDto } from './sign-in.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
-export class SignUpDto extends SignInDto {}
+export class SignUpDto {
+  @ApiProperty({
+    description: 'email',
+    type: 'string',
+    example: 'qwe@asd.com',
+    required: true,
+  })
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @ApiProperty({
+    description: 'password',
+    type: 'string',
+    example: 'password',
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
